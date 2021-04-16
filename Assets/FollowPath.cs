4f9c5d2bed9b96927f5e52cb4aae4d2e2a3b1909
Gabriel <50453714+GabrielLeandroSilva@users.noreply.github.com>
2021-04-16 17:58:24 -0300

    Add the NavMesh in the project

--- Assets/FollowPath.cs
+++ Assets/FollowPath.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FollowPath : MonoBehaviour
 {
     //Posição do objetivo
     Transform goal;
+
+    //Movimentação através do NavMesh
+    NavMeshAgent nav;
 
     // Velocidade para deslocamento
     float speed= 5.0f;
@@ -30,65 +34,71 @@ public class FollowPath : MonoBehaviour
     {
         // Obtem os pontos salvos em WPManager e passa para WPS
         wps = wpManager.GetComponent<WPManager>().waypoints;
-        g = wpManager.GetComponent<WPManager>().graph;
+
+        nav = GetComponent<NavMeshAgent>();
+        //g = wpManager.GetComponent<WPManager>().graph;
 
         // Inicia com o valor 0
-        currentNode = wps[0];
+        //currentNode = wps[0];
     }
 
 
     private void LateUpdate()
     {
         // Se for igual 0 ele irá retornar.
-        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+       // if (g.getPathLength() == 0 || currentWP == g.getPathLength())
             return;
 
         //O nó que estará mais próximo neste momento
-        currentNode = g.getPathPoint(currentWP);
+       // currentNode = g.getPathPoint(currentWP);
 
         //se estivermos mais próximo bastante do nó o tanque se moverá para o próximo
-        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy)
-        {
-            currentWP++;
-        }
+        //if (Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy)
+        //{
+        //    currentWP++;
+        //}
 
         // Será responsavel para alterar para o proximo destino
-        if (currentWP < g.getPathLength())
-        {
-            goal = g.getPathPoint(currentWP).transform;
-            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
-            Vector3 direction = lookAtGoal - this.transform.position;
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
-            this.transform.position = Vector3.MoveTowards(transform.position, goal.position, Time.deltaTime * speed);
-        }
+        //if (currentWP < g.getPathLength())
+        //{
+        //    goal = g.getPathPoint(currentWP).transform;
+        //    Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+        //    Vector3 direction = lookAtGoal - this.transform.position;
+        //    this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+        //    this.transform.position = Vector3.MoveTowards(transform.position, goal.position, Time.deltaTime * speed);
+        //}
     }
 
 
     // Metodo para navegação até o heliporto no mapa
     public void GoToHeli() 
-    { 
-        g.AStar(currentNode, wps[0]); 
-        currentWP = 0;
+    {
+        nav.SetDestination(wps[0].transform.position);
+        //g.AStar(currentNode, wps[0]); 
+        //currentWP = 0;
     }
 
     //Metodo para navegação até as ruinas no mapa
     public void GoToRuin() 
-    { 
-        g.AStar(currentNode, wps[10]); 
-        currentWP = 0;
+    {
+        nav.SetDestination(wps[10].transform.position);
+        //g.AStar(currentNode, wps[10]); 
+        //currentWP = 0;
     }
 
     // Metodo para navegação até os tanques no mapa
     public void GoToTanques()
     {
-        g.AStar(currentNode, wps[6]);
-        currentWP = 0;
+        nav.SetDestination(wps[6].transform.position);
+        //g.AStar(currentNode, wps[6]);
+        //currentWP = 0;
     }
 
     public void GoToRefinery()
     {
-        g.AStar(currentNode, wps[4]);
-        currentWP = 0;
+        nav.SetDestination(wps[4].transform.position);
+        //g.AStar(currentNode, wps[4]);
+        //currentWP = 0;
     }
 
 }
